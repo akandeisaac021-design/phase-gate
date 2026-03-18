9e162d0802c4638c69be4263fac0ecf2e73a54a6
akandeisaac021-design <akandeisaac021@gmail.com>
2026-03-18 17:11:43 +0100

    day 3

--- python/gate_three.py/task_one.py
+++ python/gate_three.py/task_one.py
@@ -1,35 +1,91 @@
-def new_list_functions(list_of_numbers, number):
+#def new_list_functions(list_of_numbers, number):
+#
+#
+#    default_value =-1
+#    length_of_list_of_numbers =len(list_of_numbers)
+#
+#
+#    new_list_of_numbers =[]
+#
+#
+#    for index in range(0, length_of_list_of_numbers):
+#        new_list_of_numbers.append(list_of_numbers[index])
+#        if (index ==number-1):
+#            return new_list_of_numbers      
+#
+#
+#
+#    length_of_new_list_of_numbers =len(new_list_of_numbers)
+#
+#
+#
+#    if (length_of_new_list_of_numbers < number):
+#
+#
+#        for _ in range(length_of_new_list_of_numbers, number):
+#            new_list_of_numbers.append(default_value)
+#
+#    return new_list_of_numbers
+#
+#
 
 
-    default_value =-1
-    length_of_list_of_numbers =len(list_of_numbers)
+
+def case_sensitive_word_counter(name):
+
+    lower_case_alphabet ="abcdefghijklmnopqrstuvwxyz"
+    upper_case_alphabet ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    characters_in_name =[]
+    characters_in_name =name
+
+    characters_in_lower_case_alphabet =[]
+    characters_in_lower_case_alphabet =lower_case_alphabet
+
+    index_of_lower_case_list =0
+    index_of_list_name =0
+
+    count =0
+
+    output =""
+
+    while (index_of_lower_case_list <26):
+
+        for character in name:
+            if characters_in_lower_case_alphabet[index_of_lower_case_list] ==characters_in_name[index_of_list_name]:
+                count +=1
+            index_of_list_name +=1
+        if count >0 :
+           output =f"{output} {characters_in_lower_case_alphabet[index_of_lower_case_list]}{count}"
+        index_of_lower_case_list +=1
+        index_of_list_name =0
+        count =0
+        
+    characters_in_upper_case_alphabet =[]
+    characters_in_upper_case_alphabet =upper_case_alphabet
+
+    index_of_upper_case_list =0
+    index_of_list_name =0
+
+    count =0
 
 
-    new_list_of_numbers =[]
+    while (index_of_upper_case_list <26):
 
-
-    for index in range(0, length_of_list_of_numbers):
-        new_list_of_numbers.append(list_of_numbers[index])
-        if (index ==number-1):
-            return new_list_of_numbers      
-
-
-
-    length_of_new_list_of_numbers =len(new_list_of_numbers)
-
-
-
-    if (length_of_new_list_of_numbers < number):
-
-
-        for _ in range(length_of_new_list_of_numbers, number):
-            new_list_of_numbers.append(default_value)
-
-    return new_list_of_numbers
+        for character in name:
+            if characters_in_upper_case_alphabet[index_of_upper_case_list] ==characters_in_name[index_of_list_name]:
+                count +=1
+            index_of_list_name +=1
+        if count >0 :
+           output =f"{output} {characters_in_upper_case_alphabet[index_of_upper_case_list]}{count}"
+        index_of_upper_case_list +=1
+        index_of_list_name =0
+        count =0
 
 
 
-number =7
 
-list_of_numbers =[1, 21, 3, 32, 54]
-print(new_list_functions(list_of_numbers, number))
+    return output
+
+name ="Isaac"
+print(case_sensitive_word_counter(name))
--- python/gate_three.py/task_one_test.py
+++ python/gate_three.py/task_one_test.py
@@ -1,41 +1,52 @@
 from unittest import TestCase
 
-from task_one import new_list_functions
+from task_one import *
 
 class test_new_list_functions(TestCase):
+#
+#    def test_that_new_list_functions_is_accurate_if_new_list_length_is_excess(self):
+#
+#        length_of_expected_list =7
+#
+#        list_of_numbers =[1, 21, 3, 32, 54]
+#
+#        actual =new_list_functions(list_of_numbers, length_of_expected_list)
+#
+#        expected =[1, 21, 3, 32, 54, -1, -1]
+#
+#        self.assertEqual(actual, expected)
+#
+#    def test_that_new_list_functions_is_accurate_if_new_list_length_is_not_enough(self):
+#
+#        length_of_expected_list =2
+#
+#        list_of_numbers =[1, 21, 3, 32, 54]
+#
+#        actual =new_list_functions(list_of_numbers, length_of_expected_list)
+#
+#        expected =[1, 21]
+#
+#        self.assertEqual(actual, expected)
+#
+#    def test_that_new_list_functions_is_accurate(self):
+#        
+#        length_of_expected_list =5
+#
+#        list_of_numbers =[1, 21, 3, 32, 54]
+#
+#        actual =new_list_functions(list_of_numbers, length_of_expected_list)
+#
+#        expected =[1, 21, 3, 32, 54]
+#
+#        self.assertEqual(actual, expected)
+#
+#
+    def test_that_case_sensitive_word_counter_accurate(self):
 
-    def test_that_new_list_functions_is_accurate_if_new_list_length_is_excess(self):
+        name ="Isaac"
 
-        length_of_expected_list =7
+        actual =case_sensitive_word_counter(name)
 
-        list_of_numbers =[1, 21, 3, 32, 54]
-
-        actual =new_list_functions(list_of_numbers, length_of_expected_list)
-
-        expected =[1, 21, 3, 32, 54, -1, -1]
-
-        self.assertEqual(actual, expected)
-
-    def test_that_new_list_functions_is_accurate_if_new_list_length_is_not_enough(self):
-
-        length_of_expected_list =2
-
-        list_of_numbers =[1, 21, 3, 32, 54]
-
-        actual =new_list_functions(list_of_numbers, length_of_expected_list)
-
-        expected =[1, 21]
-
-        self.assertEqual(actual, expected)
-
-    def test_that_new_list_functions_is_accurate(self):
-        
-        length_of_expected_list =5
-
-        list_of_numbers =[1, 21, 3, 32, 54]
-
-        actual =new_list_functions(list_of_numbers, length_of_expected_list)
-
-        expected =[1, 21, 3, 32, 54]
+        expected =" a2 c1 s1 I1"
 
         self.assertEqual(actual, expected)
