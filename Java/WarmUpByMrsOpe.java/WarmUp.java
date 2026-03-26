69869c913aca1603cc57bcc70057077f972d6e8f
akandeisaac021-design <akandeisaac021@gmail.com>
2026-03-26 10:31:08 +0100

    Mrs Ope

---
+++ Java/WarmUpByMrsOpe.java/WarmUp.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class WarmUp {
+
+    public static void removeDuplicateNumbersFromAnArray(int [] numbers){
+
+        ArrayList <Integer> nonDuplicateNumbers =new ArrayList<>();
+        
+        int lengthOfNumbers =numbers.length;
+        for (int numberToCompareIndex =0; numberToCompareIndex <lengthOfNumbers; numberToCompareIndex++){
+
+            int count =0;
+
+            for (int numberToBeComparedsIndex =0; numberToBeComparedsIndex <lengthOfNumbers; numberToBeComparedsIndex++){
+
+
+                if(numbers[numberToCompareIndex] ==numbers[numberToBeComparedsIndex]){
+                    count++;
+                }
+
+            }
+        
+
+        if (count ==0){
+            nonDuplicateNumbers.add(numbers[numberToCompareIndex]);
+        }
+
+        }
+    }
+
+}
+
+
